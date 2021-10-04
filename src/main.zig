06b7a89f3522b9d53908a5ae76444bae0dc79cd2
Frank Denis <github@pureftpd.org>
2021-10-05 01:36:11 +0200

    Overwrite hostname

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const zigly = @import("zigly.zig");
 fn start() !void {
     try zigly.compatibilityCheck();
     var downstream = try zigly.downstream();
-    try downstream.proxy("mybackend", null);
+    try downstream.proxy("mybackend", "ziglang.org");
 }
 
 pub export fn _start() callconv(.C) void {
